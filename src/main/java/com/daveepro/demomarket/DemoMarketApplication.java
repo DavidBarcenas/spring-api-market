ecd03cd49a2d8f56151c9817b2988471f9c6c452
David Barcenas <daveepro@outlook.com>
2020-09-08 20:31:50 -0500

    Create first controller

---
+++ src/main/java/com/daveepro/demomarket/DemoMarketApplication.java
@@ -0,0 +1,13 @@
+package com.daveepro.demomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daveepro/demomarket/DemoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daveepro.demomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
