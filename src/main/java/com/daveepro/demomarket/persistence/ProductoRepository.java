76604b00ad8278c71544de7e19e191cf56821307
David Barcenas <daveepro@outlook.com>
2020-09-09 21:37:58 -0500

    Query Methods

--- src/main/java/com/daveepro/demomarket/persistence/ProductoRepository.java
+++ src/main/java/com/daveepro/demomarket/persistence/ProductoRepository.java
@@ -4,11 +4,20 @@ import com.daveepro.demomarket.persistence.crud.ProductoCrudRepository;
 import com.daveepro.demomarket.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
         return (List<Producto>)productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRepository.findByCantidadStockLeesThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/daveepro/demomarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/daveepro/demomarket/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,12 @@ package com.daveepro.demomarket.persistence.crud;
 import com.daveepro.demomarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLeesThanAndEstado(int cantidadStock, boolean estado);
+
 }
