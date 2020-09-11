6bee2d70033e002838729e7b20458e2bb5a24473
David Barcenas <daveepro@outlook.com>
2020-09-10 20:45:53 -0500

    Dependency injection @Autowired

--- src/main/java/com/daveepro/demomarket/persistence/ProductoRepository.java
+++ src/main/java/com/daveepro/demomarket/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.daveepro.demomarket.domain.repository.ProductRepository;
 import com.daveepro.demomarket.persistence.crud.ProductoCrudRepository;
 import com.daveepro.demomarket.persistence.entity.Producto;
 import com.daveepro.demomarket.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,9 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
 
     @Override
