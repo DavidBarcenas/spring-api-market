49e7906bbe0c6ef9889378fae2da618b69878dbf
David Barcenas <daveepro@outlook.com>
2020-09-11 21:46:23 -0500

    Auth with JWT

--- src/main/java/com/daveepro/demomarket/web/security/SecurityConfig.java
+++ src/main/java/com/daveepro/demomarket/web/security/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.daveepro.demomarket.web.security;
 
 import com.daveepro.demomarket.domain.service.DaveeproUserDetailsService;
+import com.daveepro.demomarket.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -12,8 +18,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DaveeproUserDetailsService daveeproUserDetailsService;
 
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(daveeproUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+                .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
