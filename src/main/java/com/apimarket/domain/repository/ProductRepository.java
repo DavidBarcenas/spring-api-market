d79a948bd4491401460493a4a977aee9dec4980c
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-06 11:22:22 -0500

    create product and category mapper [mapstruct]

--- src/main/java/com/apimarket/domain/repository/ProductRepository.java
+++ src/main/java/com/apimarket/domain/repository/ProductRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface ProductRepository {
     List<Product> getAll();
     Optional<List<Product>> getByCategory(int categoryId);
-    Optional<List<Product>> getScarseProducts(int quantity);
+    Optional<List<Product>> getScarceProducts(int quantity);
     Optional<Product> getProduct(int productId);
     Product save(Product product);
     void delete(int productId);
