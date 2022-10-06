79a9106ecc72c15e1189b32f42ff64be8bb3635a
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-05 19:14:43 -0500

    create product query methods

--- src/main/java/com/apimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/apimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,10 @@ package com.apimarket.persistence.crud;
 import com.apimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
