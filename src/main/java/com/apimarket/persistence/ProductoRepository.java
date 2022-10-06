4acca1bd6cea3bffad0a891ffcef78d4687fa5ce
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-06 09:38:18 -0500

    implement the @Repository annotation

--- src/main/java/com/apimarket/persistence/ProductoRepository.java
+++ src/main/java/com/apimarket/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.apimarket.persistence;
 
 import com.apimarket.persistence.crud.ProductoCrudRepository;
 import com.apimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProudcto) {
+        productoCrudRepository.deleteById(idProudcto);
     }
 }
