61555b460c90288768183cf0ae1733ef6781c726
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-06 15:21:46 -0500

    create a domain service for the products

--- src/main/java/com/apimarket/persistence/ProductoRepository.java
+++ src/main/java/com/apimarket/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.apimarket.domain.repository.ProductRepository;
 import com.apimarket.persistence.crud.ProductoCrudRepository;
 import com.apimarket.persistence.entity.Producto;
 import com.apimarket.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,9 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
 
     @Override
