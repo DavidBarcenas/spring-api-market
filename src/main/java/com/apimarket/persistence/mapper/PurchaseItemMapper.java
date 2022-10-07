42b9d3959a93b231bbeeb3a6da19c5f474e7eef6
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-07 17:02:29 -0500

    map purchase domain

---
+++ src/main/java/com/apimarket/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,26 @@
+package com.apimarket.persistence.mapper;
+
+import com.apimarket.domain.PurchaseItem;
+import com.apimarket.persistence.entity.ComprasProducto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface PurchaseItemMapper {
+    @Mappings({
+            @Mapping(source = "id.idProducto", target = "productId"),
+            @Mapping(source = "cantidad", target = "quantity"),
+            @Mapping(source = "estado", target = "active"),
+    })
+    PurchaseItem toPurchaseItem(ComprasProducto producto);
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "compra", ignore = true),
+            @Mapping(target = "producto", ignore = true),
+            @Mapping(target = "id.idCompra", ignore = true),
+    })
+    ComprasProducto toComprasProducto(PurchaseItem item);
+}
---
+++ src/main/java/com/apimarket/persistence/mapper/PurchaseMapper.java
@@ -0,0 +1,29 @@
+package com.apimarket.persistence.mapper;
+
+import com.apimarket.domain.Purchase;
+import com.apimarket.persistence.entity.Compra;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {PurchaseItemMapper.class})
+public interface PurchaseMapper {
+    @Mappings({
+            @Mapping(source = "idCompra", target = "purchaseId"),
+            @Mapping(source = "idCliente", target = "clientId"),
+            @Mapping(source = "fecha", target = "date"),
+            @Mapping(source = "estado", target = "state"),
+            @Mapping(source = "medioPago", target = "paymentMethod"),
+            @Mapping(source = "comentario", target = "comment"),
+            @Mapping(source = "productos", target = "items"),
+    })
+    Purchase toPurchase(Compra compra);
+    List<Purchase> toPurchases(List<Compra> compras);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "cliente", ignore = true)
+    Compra toCompra(Purchase purchase);
+}
