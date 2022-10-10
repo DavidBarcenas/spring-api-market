13eb9e3e702e261d11c9c9ae8dbf9c43f4d641e2
David Barcenas <davidbarcenasmx@gmail.com>
2022-10-10 14:56:12 -0500

    generate a JWT

---
+++ src/main/java/com/apimarket/web/security/JWTUtil.java
@@ -0,0 +1,20 @@
+package com.apimarket.web.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class JWTUtil {
+    private static final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(key).compact();
+    }
+}
